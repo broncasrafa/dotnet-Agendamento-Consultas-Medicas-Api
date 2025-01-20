c1b2bbbd328e7706899457cdf4bdf4c624c3a9d4
Rafael Francisco <broncasrafa@gmail.com>
2025-01-20 10:16:43 -0300

    👉🏾 4. Fluxo de Confirmação de Agendamento de Consultas: Confirmação pelo Especialista

---
+++ src/RSF.AgendamentoConsultas.Application/Features/Agendamento/Command/ConfirmAgendamentoByEspecialista/ConfirmAgendamentoByEspecialistaRequest.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using OperationResult;
+
+namespace RSF.AgendamentoConsultas.Application.Features.Agendamento.Command.ConfirmAgendamentoByEspecialista;
+
+public record ConfirmAgendamentoByEspecialistaRequest(int AgendamentoId, int EspecialistaId) : IRequest<Result<bool>>;
